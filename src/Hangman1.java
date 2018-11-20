71f25781f881b454c6c4027b756ebc59ec7bf5ba
Unknown <steven.washington.2021@student.lhschools.org>
2018-11-20 13:56:01 -0500

    update

--- src/Hangman1.java
+++ src/Hangman1.java
@@ -1,19 +1,23 @@
+import java.util.Scanner;
+
 public class Hangman1 {
 
 public static void main(String[]args){
-    String[]phrase = {"Hello", "World"};
-    //repeatPrint("-", 2);
+    Scanner keyboard;
 
-}
+    //initializing
+    String Player1;
+    String Player2;
+    String letter;
+    String[] currentGuessbreakdown;
 
+    keyboard = new Scanner(System.in);
 
-
-    public static void repeatPrint(String message, int maxValue){
-    for(int potato=0;potato<maxValue;potato++){
-        System.out.println(message);
-        for(int innerLoop = 0; innerLoop<5; innerLoop++){
-            System.out.println(message);
+    System.out.println("Hello there and WELCOME to The Hangman Game");
+    System.out.println("Here are the rules, you can guess as many times as you want but you get 7 mistakes.");
+    System.out.println("Guess over 7 mistakes and you get it wrong.");
+    System.out.println("Lets Begin!");
         }
     }
-}
-}
+
+
